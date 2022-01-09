3ef77adcb0c69bff98052e7ee870d37e17511db3
Crowdbotics <team@crowdbotics.com>
2022-01-09 00:13:06 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -23,12 +23,26 @@ const styles = StyleSheet.create({
   scrollview: { flex: 1, padding: 20 },
   header: { color: "#fff", fontSize: 16 }
 })
+
 export const globalOptions = {
   name: "par_tee_32752",
   url: "https://par_tee_32752.botics.co",
   api: "https://par_tee_32752.botics.co/api/v1"
 }
+
 export const modulesOptions = {
   "@modules/app-menu": { copy: "Routes available!" },
-  "@modules/terms-and-conditions": { title: title, path: path, styles: styles }
+  "@modules/terms-and-conditions": { title: title, path: path, styles: styles },
+  "@modules/login": {
+    LOGO_URL:
+      "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/cb-icon.png",
+    BACKGROUND_URL:
+      "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/halfbg.png",
+    HOME_SCREEN_NAME: "Home Screen",
+    validateEmail: "^[^\\s]+([.-]?\\w+)*@\\w+([.-]?\\w+)*(.\\w{2,3})+$",
+    SignInNavText: "Sign In",
+    SignUpNavText: "Sign Up",
+    SignInButtonText: "Login",
+    SignUpButtonText: "Sign Up"
+  }
 }
